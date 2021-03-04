f0fe9f88afb26f1be3e38c2338e99a9706cfbd84
Michael Ryan <michaelryan2895@gmail.com>
2021-03-03 19:17:29 -0500

    api js array

--- js/quote-api.js
+++ js/quote-api.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const url= "http://api.forismatic.com/api/1.0/method=getQuote&key=457653&format=json&lang=en";
-// const params= {
-//     method: "getQuote", 
-//     key: 457653,
-//     format: "JSON",
-//     lang: "en",
-// }
-axios.get (url)
-.then (data=>console.log(data))
-.catch (err=>console.log(err))
+const url = "https://quote-garden.herokuapp.com/api/v3/quotes";
+let quotes = [];
+axios.get(url, {
+    params: {
+        genre: "inspirational",
+        page: 2,
+    }
+})
+    .then(response => {
+        // console.log(response.data.data);
+        let data = response.data;
+        for (let i = 0; i < 10; i++) {
+        let quote = response.data.data[i];
+        // console.log(quote.quoteAuthor)
+        // console.log(quote.quoteText)
+        quotes.push([quote.quoteAuthor, quote.quoteText])
+        }
 
+
+
+    })
+    .catch(err => console.log(err))
+    console.log(quotes)
